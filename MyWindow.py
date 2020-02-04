fae44b941d06fdd62e02f4e1f31f13ad0dfae88e
wer010 <yuri924@163.com>
2020-02-05 01:47:28 +0800

    2/5 commit

--- MyWindow.py
+++ MyWindow.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QSlider,QGroupBox,QPushButton, QFileDialog, QWidget, QVBoxLayout, QHBoxLayout, QSplitter, QLabel
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QLineEdit,QSlider,QGroupBox,QPushButton, QFileDialog, QWidget, QVBoxLayout, QHBoxLayout, QSplitter, QLabel
+from PyQt5.QtGui import QPixmap,QIntValidator
 
 
 class mywindow(QWidget):
@@ -9,9 +9,9 @@ class mywindow(QWidget):
     #     self.setupui()
     #
     # def setupui(self):
-        total_area = QHBoxLayout()
-        self.setLayout(total_area)
-        self.setGeometry(300, 300, 800, 600)
+        total_arealayout = QHBoxLayout()
+        self.setLayout(total_arealayout)
+        self.setGeometry(50, 50, 800, 600)
         self.setWindowTitle('图像处理')
 
         a_area = QGroupBox()
@@ -22,7 +22,9 @@ class mywindow(QWidget):
         b_area = QGroupBox()
         b_arealayout = QVBoxLayout()
         b_area.setLayout(b_arealayout)
+        bt_area = QWidget()
         bt_arealayout = QHBoxLayout()
+        bt_area.setLayout(bt_arealayout)
 
         self.bb_area = QLabel()
         self.bb_area.setText('Picture')
@@ -40,21 +42,40 @@ class mywindow(QWidget):
         b1 = QLabel()
         b1.setText("模糊系数")
         b2 = QSlider(Qt.Horizontal)
+        b2.setMinimum(0)
+        b2.setMaximum(100)
+        b2.setValue(50)
+
+        b3 = QLineEdit()
+        b3.setValidator(QIntValidator())
+        b3.setText("25")
+
+        b4 = QLabel()
+
 
 
         a_arealayout.addWidget(a1)
         a_arealayout.addWidget(a2)
         a_arealayout.addWidget(a3)
         bt_arealayout.addWidget(b1)
+        bt_arealayout.setStretchFactor(b1,1)
         bt_arealayout.addWidget(b2)
+        bt_arealayout.setStretchFactor(b2,1)
+        bt_arealayout.addWidget(b3)
+        bt_arealayout.setStretchFactor(b3,1)
+        bt_arealayout.addWidget(b4)
+        bt_arealayout.setStretchFactor(b4,4)
 
-        b_arealayout.addLayout(bt_arealayout)
+
+        b_arealayout.addWidget(bt_area)
         b_arealayout.addWidget(self.bb_area)
+        b_arealayout.setStretchFactor(bt_area,3)
+        b_arealayout.setStretchFactor(self.bb_area, 7)
 
-        total_area.addWidget(a_area)
-        total_area.addWidget(b_area)
-        total_area.setStretchFactor(a_area, 3)
-        total_area.setStretchFactor(b_area, 7)
+        total_arealayout.addWidget(a_area)
+        total_arealayout.addWidget(b_area)
+        total_arealayout.setStretchFactor(a_area, 2)
+        total_arealayout.setStretchFactor(b_area, 8)
 
 
 
